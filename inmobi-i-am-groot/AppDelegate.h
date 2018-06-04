7893bdfa87fb267ef21b2ea01556df477c4cf6c4
Jason C <jforeza@gmail.com>
2018-06-04 11:24:53 -0700

    Initial Commit

---
+++ inmobi-i-am-groot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  inmobi-i-am-groot
+//
+//  Created by Jason C on 6/4/18.
+//  Copyright © 2018 Jason C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ inmobi-i-am-groot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  inmobi-i-am-groot
+//
+//  Created by Jason C on 6/4/18.
+//  Copyright © 2018 Jason C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
